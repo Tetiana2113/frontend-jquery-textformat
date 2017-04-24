942748543d31d4ebdcbe38998949ad25da5350fe
tetiana sorochynska <tsorochynska2113@gmail.com>
2017-04-24 09:14:26 -0400

    add function to change the value of an input element

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 'use strict';
-
+let textarea =$('message-field');
+let output =$('message-formatted');
+input.on('message-formatted', function(){
+  let value = textarea.val();
+//   $.replace(/\s+/g, ' ')
+//   $.trim(value);
+//   $.toLowerCase();
+  output.text(value);
+})
 console.log($)
